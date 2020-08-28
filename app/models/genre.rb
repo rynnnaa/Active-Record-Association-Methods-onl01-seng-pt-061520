24ba933b84ab4a3425dd58177be44bc743bdf866
Ryna Truong <rynatruong@outlook.com>
2020-08-28 15:40:32 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -15,7 +15,6 @@ class Genre < ActiveRecord::Base
   def all_artist_names
     artists.all.map do |a|
     a.name
-  end
     # return an array of strings containing every musician's name
   end
 end
